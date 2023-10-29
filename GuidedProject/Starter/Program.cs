2546d494827901655b9c94bb198b37ffab490b8c
Simon Rohrer <105282514+rohrersimon@users.noreply.github.com>
2023-10-29 21:31:19 +0000

    commit

--- GuidedProject/Starter/Program.cs
+++ GuidedProject/Starter/Program.cs
@@ -86,8 +86,8 @@ do
     Console.WriteLine(" 2. Add a new animal friend to the ourAnimals array");
     Console.WriteLine(" 3. Ensure animal ages and physical descriptions are complete");
     Console.WriteLine(" 4. Ensure animal nicknames and personality descriptions are complete");
-    Console.WriteLine(" 5. Edit an animal’s age");
-    Console.WriteLine(" 6. Edit an animal’s personality description");
+    Console.WriteLine(" 5. Edit an animal's age");
+    Console.WriteLine(" 6. Edit an animal's personality description");
     Console.WriteLine(" 7. Display all cats with a specified characteristic");
     Console.WriteLine(" 8. Display all dogs with a specified characteristic");
     Console.WriteLine();
